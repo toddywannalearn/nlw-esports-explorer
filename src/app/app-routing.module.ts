0b1e2912b6424f1eccb69526dd9656a1576ecf6d
Douglas <toddynotes@gmail.com>
2022-09-14 01:39:15 -0300

    feat(landing): estrutura inicial + avatar e card meus jogos

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {path: '', component: LandingPageComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
